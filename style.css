5d976973d418940ea76b3f83120a575a6de6ef0f
Mobina <129074935+MobinaMzh@users.noreply.github.com>
2025-02-26 09:28:28 -0500

    Update style.css

--- style.css
+++ style.css
@@ -1,1 +1,11 @@
-
+body {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    background-color: #f5f5f5;
+    margin: 0;
+}
+header {
+    background: #333;
+    color: white;
+    padding: 20px;
+}
